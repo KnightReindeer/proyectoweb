00172c78c74f0a22ec670f41ab8e2e027170a387
codeneomatrix <josuecevedo@gmail.com>
2015-05-16 22:19:02 -0500

    implementacion
    el proyecto se ha adecuado a un despliege en openshift

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+import os
+
+# Put here required packages
+packages = ['Django<=1.6','Unipath==0.2.1','Pillow==2.3.0',]
+
+
+if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
+     packages.append('django-redis-cache')
+     packages.append('hiredis')
+
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='https://pypi.python.org/pypi',
+      install_requires=packages,
+)
+
